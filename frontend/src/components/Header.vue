83977839f28d396edc56a9070bc050fd8779ac2b
TedEngineer <tedvu184@gmail.com>
2023-03-05 15:03:39 +1100

    Add navigation drawer

--- frontend/src/components/Header.vue
+++ frontend/src/components/Header.vue
@@ -1,12 +1,39 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { ref } from "vue";
+
+const drawer = ref(false);
+</script>
 
 <template>
-  <v-layout>
-    <v-app-bar color="info" density="comfortable">
-      <v-app-bar-nav-icon></v-app-bar-nav-icon>
-      <v-app-bar-title>My Subscription</v-app-bar-title>
-    </v-app-bar>
-  </v-layout>
+  <v-card>
+    <v-layout>
+      <v-app-bar color="info" density="comfortable">
+        <v-app-bar-nav-icon @click.stop="drawer = !drawer"></v-app-bar-nav-icon>
+        <v-app-bar-title>My Subscription</v-app-bar-title>
+      </v-app-bar>
+      <v-navigation-drawer v-model="drawer" temporary>
+        <v-list-item
+          prepend-avatar="https://randomuser.me/api/portraits/men/78.jpg"
+          title="John Leider"
+        ></v-list-item>
+
+        <v-divider></v-divider>
+
+        <v-list density="compact" nav>
+          <v-list-item
+            prepend-icon="mdi-view-dashboard"
+            title="Home"
+            value="home"
+          ></v-list-item>
+          <v-list-item
+            prepend-icon="mdi-forum"
+            title="About"
+            value="about"
+          ></v-list-item>
+        </v-list>
+      </v-navigation-drawer>
+    </v-layout>
+  </v-card>
 </template>
 
 <style></style>
--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -1,15 +1,19 @@
+// Vue
 import { createApp } from "vue";
-import "./style.css";
 import App from "./App.vue";
-import "@mdi/font/css/materialdesignicons.css";
+// Pages
 import About from "./Pages/About.vue";
 import Home from "./Pages/Home.vue";
 
-// Vuetify
+// Vuetify and styling
 import "vuetify/styles";
 import { createVuetify } from "vuetify";
 import * as components from "vuetify/components";
 import * as directives from "vuetify/directives";
+import "@mdi/font/css/materialdesignicons.css";
+import "./style.css";
+
+// Vue router
 import * as VueRouter from "vue-router";
 
 const vuetify = createVuetify({
@@ -22,13 +26,9 @@ const routes = [
   { path: "/about", component: About },
 ];
 
-// 3. Create the router instance and pass the `routes` option
-// You can pass in additional options here, but let's
-// keep it simple for now.
 const router = VueRouter.createRouter({
-  // 4. Provide the history implementation to use. We are using the hash history for simplicity here.
   history: VueRouter.createWebHistory(),
-  routes, // short for `routes: routes`
+  routes,
 });
 
 createApp(App).use(vuetify).use(router).mount("#app");
