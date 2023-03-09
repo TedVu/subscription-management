3a2c67dbffa526667cf4817be148378a9bd88360
TedEngineer <tedvu184@gmail.com>
2023-03-09 22:11:18 +1100

    Fix defect header and items are displayed on top of each other

--- frontend/src/components/Header.vue
+++ frontend/src/components/Header.vue
@@ -18,43 +18,41 @@ const handleNavigation = (page: String) => {
 </script>
 
 <template>
-  <v-card>
-    <v-layout>
-      <v-app-bar color="info" density="comfortable">
-        <v-app-bar-nav-icon @click.stop="drawer = !drawer"></v-app-bar-nav-icon>
-        <v-app-bar-title>Subscription Management Application</v-app-bar-title>
-      </v-app-bar>
-      <v-navigation-drawer v-model="drawer" temporary>
+  <v-layout>
+    <v-app-bar color="info" density="comfortable">
+      <v-app-bar-nav-icon @click.stop="drawer = !drawer"></v-app-bar-nav-icon>
+      <v-app-bar-title>Subscription Management Application</v-app-bar-title>
+    </v-app-bar>
+    <v-navigation-drawer v-model="drawer" temporary>
+      <v-list-item
+        prepend-avatar="https://tedvublogimages.s3.us-west-1.amazonaws.com/profilepics.jpg"
+        title="Ted Vu"
+      ></v-list-item>
+
+      <v-divider></v-divider>
+
+      <v-list density="compact" nav>
         <v-list-item
-          prepend-avatar="https://tedvublogimages.s3.us-west-1.amazonaws.com/profilepics.jpg"
-          title="Ted Vu"
+          prepend-icon="mdi-view-dashboard"
+          title="Home"
+          value="home"
+          @click="handleNavigation('Home')"
         ></v-list-item>
-
-        <v-divider></v-divider>
-
-        <v-list density="compact" nav>
-          <v-list-item
-            prepend-icon="mdi-view-dashboard"
-            title="Home"
-            value="home"
-            @click="handleNavigation('Home')"
-          ></v-list-item>
-          <v-list-item
-            prepend-icon="mdi-forum"
-            title="About"
-            value="about"
-            @click="handleNavigation('About')"
-          ></v-list-item>
-          <v-list-item
-            prepend-icon="mdi-account"
-            title="New Subscription"
-            value="New Subscription"
-            @click="handleNavigation('NewSubcription')"
-          ></v-list-item>
-        </v-list>
-      </v-navigation-drawer>
-    </v-layout>
-  </v-card>
+        <v-list-item
+          prepend-icon="mdi-forum"
+          title="About"
+          value="about"
+          @click="handleNavigation('About')"
+        ></v-list-item>
+        <v-list-item
+          prepend-icon="mdi-account"
+          title="New Subscription"
+          value="New Subscription"
+          @click="handleNavigation('NewSubcription')"
+        ></v-list-item>
+      </v-list>
+    </v-navigation-drawer>
+  </v-layout>
 </template>
 
 <style></style>
