a6fefa5a3e12857b08c0a285a5e780fce16ceeb6
TedEngineer <tedvu184@gmail.com>
2023-03-16 22:06:43 +1100

    Move subscription items up the component tree

--- frontend/src/Pages/Home.vue
+++ frontend/src/Pages/Home.vue
@@ -1,9 +1,38 @@
 <script setup lang="ts">
+import { onMounted, ref } from "vue";
 import Header from "../components/Header.vue";
+import { Card } from "../components/types";
 import SubscriptionList from "../components/SubscriptionList.vue";
+import moment from "moment";
+
+const subscriptionItems = ref<Card[]>();
+onMounted(() => {
+  subscriptionItems.value = [
+    {
+      title: "Youtube",
+      date: moment("02-02-2023").format("DD-MM-YYYY"),
+      imageUrl: "",
+    },
+    {
+      title: "Twitch",
+      date: moment("02-02-2023").format("DD-MM-YYYY"),
+      imageUrl: "",
+    },
+    {
+      title: "Twitch",
+      date: moment("02-02-2023").format("DD-MM-YYYY"),
+      imageUrl: "",
+    },
+    {
+      title: "Twitch",
+      date: moment("02-02-2023").format("DD-MM-YYYY"),
+      imageUrl: "",
+    },
+  ];
+});
 </script>
 
 <template>
   <Header />
-  <SubscriptionList />
+  <SubscriptionList :subscription-items="subscriptionItems" />
 </template>
--- frontend/src/components/SubscriptionList.vue
+++ frontend/src/components/SubscriptionList.vue
@@ -2,24 +2,27 @@
 import SubscriptionCard from "./SubscriptionCard.vue";
 import { Card } from "./types";
 import { PropType } from "vue";
-// Moment
-import moment from "moment";
 
-const props = defineProps({
-  subscriptionItem: Array as PropType<Card[]>,
+defineProps({
+  subscriptionItems: Array as PropType<Card[]>,
 });
 </script>
 
 <template>
   <v-container>
     <v-row no-gutters>
-      <v-col v-for="n in 10" :key="n" cols="12" sm="4">
+      <v-col
+        v-for="(item, index) in subscriptionItems"
+        :key="index"
+        cols="12"
+        sm="4"
+      >
         <v-sheet class="ma-2 pa-2">
           <SubscriptionCard
             :card="{
-              title: 'Youtube',
-              date: moment('02-02-2023').format('DD-MM-YYYY'),
-              imageUrl: '',
+              title: item.title,
+              date: item.date,
+              imageUrl: item.imageUrl,
             }"
           />
         </v-sheet>
