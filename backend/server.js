7f89d1a169750a5b58f2a601755dd904f60b85f8
TedEngineer <tedvu184@gmail.com>
2023-03-31 21:18:15 +1100

    Add endpoint returning a json object for subcription

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,30 @@
 const express = require("express");
+const moment = require("moment");
 const app = express();
 const port = 8080;
 
 app.get("/", (req, res) => {
   res.send({ message: "Hello World!" });
+});
+
+app.get("/test", (req, res) => {
+  res.json([
+    {
+      title: "Youtube",
+      date: moment("02-02-2023").format("DD-MM-YYYY"),
+      imageUrl: "",
+    },
+    {
+      title: "Twitch",
+      date: moment("02-02-2023").format("DD-MM-YYYY"),
+      imageUrl: "",
+    },
+    {
+      title: "Twitch",
+      date: moment("02-02-2023").format("DD-MM-YYYY"),
+      imageUrl: "",
+    },
+  ]);
 });
 
 app.listen(port, () => {
