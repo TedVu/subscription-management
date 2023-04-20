866896bd5f2b28444fe031e92593f95cebd33dbf
TedEngineer <tedvu184@gmail.com>
2023-04-20 11:30:00 +1000

    Add signin with google

--- frontend/src/Pages/Login.vue
+++ frontend/src/Pages/Login.vue
@@ -1,7 +1,37 @@
 <script setup lang="ts">
 import Header from "../components/Header.vue";
+import { getAuth, signInWithPopup, GoogleAuthProvider } from "firebase/auth";
+
+const provider = new GoogleAuthProvider();
+
+const auth = getAuth();
+
+const handleLogin = () => {
+  signInWithPopup(auth, provider)
+    .then((result) => {
+      console.log("Sign in successfully");
+      // This gives you a Google Access Token. You can use it to access the Google API.
+      const credential = GoogleAuthProvider.credentialFromResult(result);
+      const token = credential?.accessToken;
+      // The signed-in user info.
+      const user = result.user;
+      // IdP data available using getAdditionalUserInfo(result)
+      // ...
+    })
+    .catch((error) => {
+      // Handle Errors here.
+      const errorCode = error.code;
+      const errorMessage = error.message;
+      // The email of the user's account used.
+      const email = error.customData.email;
+      // The AuthCredential type that was used.
+      const credential = GoogleAuthProvider.credentialFromError(error);
+      // ...
+    });
+};
 </script>
 
 <template>
   <Header />
+  <v-btn variant="outlined" @click="handleLogin"> Login with Google </v-btn>
 </template>
