ccda7a9e495541720bb0ace7b39df00422c24a81
TedEngineer <tedvu184@gmail.com>
2023-04-22 11:12:22 +1000

    add unauthorized page

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -1,33 +1,21 @@
-// Vue
 import { createApp } from "vue";
 import App from "./App.vue";
-
-// pinia - state management
-import { createPinia, storeToRefs } from "pinia";
+import { createPinia } from "pinia";
 import { useAuthenticationStore } from "./stores/AuthenticationStore";
-
-// Pages
 import About from "./Pages/About.vue";
 import Home from "./Pages/Home.vue";
 import Login from "./Pages/Login.vue";
 import NewSubscription from "./Pages/NewSubscription.vue";
-
-// Vuetify and styling
+import Error from "./Pages/Error.vue";
 import "vuetify/styles";
 import { createVuetify } from "vuetify";
 import * as components from "vuetify/components";
 import * as directives from "vuetify/directives";
 import "@mdi/font/css/materialdesignicons.css";
 import "./style.css";
-
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   apiKey: "AIzaSyC85KMacum02NekX0TXG34LIEJRyNoqcW8",
   authDomain: "subscription-management-40f86.firebaseapp.com",
@@ -37,12 +25,8 @@ const firebaseConfig = {
   appId: "1:82518374928:web:bab204014ff0fa22eea895",
   measurementId: "G-Q5SMF2EPG6",
 };
-
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
-
-// Vue router
 import * as VueRouter from "vue-router";
 
 const vuetify = createVuetify({
@@ -51,7 +35,12 @@ const vuetify = createVuetify({
 });
 
 const routes = [
-  { path: "/", name: "Login", component: Login, meta: { requiresAuth: false } },
+  {
+    path: "/login",
+    name: "Login",
+    component: Login,
+    meta: { requiresAuth: false, loginPage: true },
+  },
   {
     path: "/home",
     name: "Home",
@@ -63,6 +52,11 @@ const routes = [
     path: "/new-subcription",
     component: NewSubscription,
     meta: { requiresAuth: true },
+  },
+  {
+    path: "/error",
+    component: Error,
+    meta: { requiresAuth: false },
   },
 ];
 
@@ -85,6 +79,7 @@ router.beforeEach((to, from, next) => {
       next(true);
     } else {
       next(false);
+      router.push("/Error");
     }
   } else {
     next(true);
