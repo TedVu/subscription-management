a54a2404bcee92530320d8fd84d31ac5c99d3e56
TedEngineer <tedvu184@gmail.com>
2023-05-24 21:29:19 +1000

    add simple subscription form

--- frontend/src/Pages/NewSubscription.vue
+++ frontend/src/Pages/NewSubscription.vue
@@ -5,14 +5,32 @@ import VueDatePicker from "@vuepic/vue-datepicker";
 import "@vuepic/vue-datepicker/dist/main.css";
 import { enAU } from "date-fns/locale";
 
-const date = ref();
+const name = ref("");
+const date = ref(null);
+const loading = ref(false);
+
+const nameRules = [
+  (name: String) => {
+    if (name?.length > 3) return true;
+
+    return "Subscription name must be at least 3 characters.";
+  },
+];
+
+const submit = async () => {
+  // submit to server
+};
 </script>
 
 <template>
   <Header />
   <v-sheet width="300" class="mx-auto">
-    <v-form @submit.prevent>
-      <v-text-field label="Subscription name"></v-text-field>
+    <v-form fast-fail validate-on="submit lazy" @submit.prevent="submit">
+      <v-text-field
+        v-model="name"
+        label="Subscription name"
+        :rules="nameRules"
+      ></v-text-field>
       <VueDatePicker
         v-model="date"
         :enable-time-picker="false"
@@ -20,7 +38,7 @@ const date = ref();
         format="dd/MM/yyyy"
       ></VueDatePicker>
 
-      <v-btn type="submit" block class="mt-2">Submit</v-btn>
+      <v-btn :loading="loading" type="submit" block class="mt-2">Submit</v-btn>
     </v-form>
   </v-sheet>
 </template>
