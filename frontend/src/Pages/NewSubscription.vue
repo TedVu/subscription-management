d856f97d3bbe985f994b1899391eb175c05f2315
TedEngineer <tedvu184@gmail.com>
2023-05-24 20:25:15 +1000

    complete new subscription page

--- frontend/src/Pages/NewSubscription.vue
+++ frontend/src/Pages/NewSubscription.vue
@@ -4,4 +4,11 @@ import Header from "../components/Header.vue";
 
 <template>
   <Header />
+  <v-sheet width="300" class="mx-auto">
+    <v-form @submit.prevent>
+      <v-text-field label="Subscription name"></v-text-field>
+
+      <v-btn type="submit" block class="mt-2">Submit</v-btn>
+    </v-form>
+  </v-sheet>
 </template>
