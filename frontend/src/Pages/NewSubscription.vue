f4ab8c1e77c353cc2a2f4b32a401607012543040
TedEngineer <tedvu184@gmail.com>
2023-05-29 22:18:21 +1000

    why not saved ? :)

--- frontend/src/Pages/NewSubscription.vue
+++ frontend/src/Pages/NewSubscription.vue
@@ -13,46 +13,48 @@ const loading = ref(false);
 const snackbar = ref(false);
 
 const nameRules = [
-  (name: String) => {
-    if (name?.length > 3) return true;
+    (name: string) => {
+        if (name?.length > 3) return true;
 
-    return "Subscription name must be at least 3 characters.";
-  },
+        return "Subscription name must be at least 3 characters.";
+    },
 ];
 
 const submit = async () => {
-  loading.value = true;
-  const { db } = useFirebaseDataStore();
-  const docRef = await addDoc(collection(db, "subscriptions"), {
-    name: name.value,
-    date: date.value,
-  });
-  console.log("Document written with ID: ", docRef.id);
-  loading.value = false;
-  snackbar.value = true;
+    loading.value = true;
+    const { db } = useFirebaseDataStore();
+    const docRef = await addDoc(collection(db, "subscriptions"), {
+        name: name.value,
+        date: date.value,
+    });
+    console.log("Document written with ID: ", docRef.id);
+    loading.value = false;
+    snackbar.value = true;
 };
 </script>
 
 <template>
-  <Header />
-  <v-sheet width="300" class="mx-auto">
-    <v-form fast-fail validate-on="submit lazy" @submit.prevent="submit">
-      <v-text-field
-        v-model="name"
-        label="Subscription name"
-        :rules="nameRules"
-      ></v-text-field>
-      <VueDatePicker
-        v-model="date"
-        :enable-time-picker="false"
-        :format-locale="enAU"
-        format="dd/MM/yyyy"
-      ></VueDatePicker>
+    <Header />
+    <v-sheet width="300" class="mx-auto">
+        <v-form fast-fail validate-on="submit" @submit.prevent="submit">
+            <v-text-field
+                v-model="name"
+                label="Subscription name"
+                :rules="nameRules"
+            ></v-text-field>
+            <VueDatePicker
+                v-model="date"
+                :enable-time-picker="false"
+                :format-locale="enAU"
+                format="dd/MM/yyyy"
+            ></VueDatePicker>
 
-      <v-btn :loading="loading" type="submit" block class="mt-2">Submit</v-btn>
-    </v-form>
-    <v-snackbar v-model="snackbar">
-      {{ "Adding new subscription successful!" }}</v-snackbar
-    >
-  </v-sheet>
+            <v-btn :loading="loading" type="submit" block class="mt-2"
+                >Submit</v-btn
+            >
+        </v-form>
+        <v-snackbar v-model="snackbar">
+            {{ "Adding new subscription successful!" }}</v-snackbar
+        >
+    </v-sheet>
 </template>
