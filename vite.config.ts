4e78a5f28424b822683400fde6c50aa73f429ce0
TedEngineer <tedvu184@gmail.com>
2023-05-30 22:11:36 +1000

    edit prettier config

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,8 @@
-import { defineConfig } from "vite";
-import vue from "@vitejs/plugin-vue";
-import eslint from 'vite-plugin-eslint'
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
+import eslint from 'vite-plugin-eslint';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue(), eslint()],
+    plugins: [vue(), eslint()],
 });
