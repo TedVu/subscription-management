15d83eb658c74107ac7b18d2d12d283fa08d9d28
TedEngineer <tedvu184@gmail.com>
2023-06-17 16:01:54 +1000

    move subscription list to global state as many component read this state

--- src/Pages/HomePage.vue
+++ src/Pages/HomePage.vue
@@ -1,42 +1,18 @@
 <script setup lang="ts">
-import { onMounted, ref } from "vue";
+import { onMounted } from "vue";
 import HeaderComponent from "../components/HeaderComponent.vue";
-import { Card } from "../components/types";
 import SubscriptionList from "../components/SubscriptionList.vue";
-import { useFirebaseDataStore } from "../firebase";
-import { Timestamp, collection, getDocs } from "firebase/firestore";
-import { v4 as uuidv4 } from "uuid";
+import { useSubscriptionItemStore } from "../stores/SubscriptionItemsStore";
 
-const subscriptionItems = ref<Card[]>();
 onMounted(() => {
-  const { db } = useFirebaseDataStore();
-  const colRef = collection(db, "subscriptions");
-
-  const subscriptionItemsDB: Card[] = [];
-  getDocs(colRef).then((snapshots) => {
-    snapshots.docs.forEach((doc) => {
-      const date = new Timestamp(
-        doc.data().date.seconds,
-        doc.data().date.nanoseconds
-      )
-        .toDate()
-        .toLocaleDateString("en-AU");
-
-      const name = doc.data().name;
-      subscriptionItemsDB.push({
-        id: uuidv4(),
-        title: name,
-        date: date,
-        imageUrl: "",
-      });
-    });
-
-    subscriptionItems.value = subscriptionItemsDB;
-  });
+  const store = useSubscriptionItemStore();
+  store.getSubscriptionItemsFromServer();
 });
 </script>
 
 <template>
   <HeaderComponent />
-  <SubscriptionList :subscription-items="subscriptionItems" />
+  <SubscriptionList
+    :subscription-items="useSubscriptionItemStore().subscriptionItems"
+  />
 </template>
--- src/stores/SubscriptionItemsStore.ts
+++ src/stores/SubscriptionItemsStore.ts
@@ -1,8 +1,11 @@
 import { defineStore } from "pinia";
+import { useFirebaseDataStore } from "../firebase";
+import { Timestamp, collection, getDocs } from "firebase/firestore";
+import { Card } from "../components/types";
+
 export const useSubscriptionItemStore = defineStore("subscription-item", {
     state: () => ({
-        /** @type {{id: string, title:string, date: string, imageUrl: string}[]} */
-        subscriptionItems: [],
+        subscriptionItems: [] as Card[],
     }),
     getters: {},
     actions: {
@@ -12,9 +15,35 @@ export const useSubscriptionItemStore = defineStore("subscription-item", {
         add() {
             //
         },
-        update() {
+        update(id: string) {
             //
         },
+        getSubscriptionItemsFromServer() {
+            const { db } = useFirebaseDataStore();
+            const colRef = collection(db, "subscriptions");
+
+            const subscriptionItemsDB: Card[] = [];
+            getDocs(colRef).then((snapshots) => {
+                snapshots.docs.forEach((doc) => {
+                    const date = new Timestamp(
+                        doc.data().date.seconds,
+                        doc.data().date.nanoseconds
+                    )
+                        .toDate()
+                        .toLocaleDateString("en-AU");
+
+                    const name = doc.data().name;
+                    const id = doc.data().id;
+                    subscriptionItemsDB.push({
+                        id: id,
+                        title: name,
+                        date: date,
+                        imageUrl: "",
+                    });
+                });
+
+                this.subscriptionItems = subscriptionItemsDB;
+            });
+        },
     },
-    persist: true,
 });
